a420ebee093d5e1a75597aa884e7068faf938c9d
Stephen Brown <stephenrbrownnb@gmail.com>
2023-04-07 11:00:14 -0300

    Updated server.js and auth.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+//This is not going to be needed anymore
 const routes = require('./routes');
 
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +23,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+//this will be taken out as well
 app.use(routes);
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+})
+};
+
+startApolloServer(typeDefs, resolvers);
